6b63a6dd90acdd5a1465e59829c8a540e4d36eeb
Pekka Enberg <penberg@iki.fi>
2016-10-28 21:22:58 +0300

    kernel: Disable interrupts in panic()

--- kernel/panic.c
+++ kernel/panic.c
@@ -3,11 +3,14 @@
 #include <kernel/console.h>
 #include <kernel/cpu.h>
 
+#include <arch/interrupts.h>
+
 void panic(char *msg)
 {
 	console_write("Kernel panic: ");
 	console_write(msg);
 	console_write("\n");
+	arch_local_interrupt_disable();
 	for (;;) {
 		arch_halt_cpu();
 	}
