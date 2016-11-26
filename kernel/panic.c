3c29ad304ab98d336cd4ef0be8f52e6665e16188
Pekka Enberg <penberg@iki.fi>
2016-11-26 09:33:39 +0200

    kernel/panic: Implement __assert_fail()
    This makes it possible to use assert() in the kernel code.

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,6 +1,7 @@
 #include <kernel/panic.h>
 
 #include <kernel/console.h>
+#include <kernel/printf.h>
 #include <kernel/cpu.h>
 
 #include <arch/interrupts.h>
@@ -14,4 +15,11 @@ void panic(char *msg)
 	for (;;) {
 		arch_halt_cpu();
 	}
+}
+
+void __assert_fail(const char *assertion, const char *file, unsigned int line, const char *function)
+{
+	static char msg[64];
+	sprintf(msg, "%s:%d: %s: Assertion %s failed", file, line, function, assertion);
+	panic(msg);
 }
