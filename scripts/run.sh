44122e321920b773ecce47ec56b4e4176cd82049
Pekka Enberg <penberg@iki.fi>
2017-01-02 21:22:25 +0200

    scripts/run.sh: Boot flat binary for AArch64
    Boot the flat binary on AArch64 so that QEMU bootloader constructs a FDT
    for us.

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ ARCH=$(readelf -h kernel.elf | grep "Machine:" | sed "s/.*Machine:\s*\(.*\)/\1/g
 
 case "$ARCH" in
     AArch64)
-        qemu-system-aarch64 -M virt -cpu cortex-a57 -nographic -kernel kernel.elf
+        qemu-system-aarch64 -M virt -cpu cortex-a57 -nographic -kernel kernel.bin
         ;;
     Advanced\ Micro\ Devices\ X86-64)
         qemu-system-x86_64 -nographic -enable-kvm -cdrom kernel.iso
