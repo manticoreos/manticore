1a56413eacc1d784d7ef203182b152483c281f1a
Pekka Enberg <penberg@iki.fi>
2017-04-14 09:17:26 +0300

    include/kernel: Add is_aligned() to align.h

--- include/kernel/align.h
+++ include/kernel/align.h
@@ -1,7 +1,13 @@
 #ifndef KERNEL_ALIGN_H
 #define KERNEL_ALIGN_H
 
+#include <stdbool.h>
 #include <stdint.h>
+
+static inline bool is_aligned(uint64_t size, uint64_t align)
+{
+	return (size % align) == 0;
+}
 
 static inline uint64_t align_down(uint64_t size, uint64_t align)
 {
