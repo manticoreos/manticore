322d82faac9a1cd118890ae8c2d8a0e4e62ecab8
Pekka Enberg <penberg@iki.fi>
2017-07-24 12:15:10 +0300

    x86: 16-bit and 32-bit I/O port accessors

--- arch/x86_64/include/arch/ioport.h
+++ arch/x86_64/include/arch/ioport.h
@@ -24,4 +24,46 @@ static inline void outb(uint8_t v, uint16_t port)
 		);
 }
 
+static inline uint16_t inw(uint16_t port)
+{
+	uint16_t v;
+
+	asm volatile (
+		"inw %1, %0"
+		:"=a" (v)
+		:"dN"(port)
+		);
+	return v;
+}
+
+static inline void outw(uint16_t v, uint16_t port)
+{
+	asm volatile (
+		"outw %0, %1"
+		:
+		:"a" (v), "dN"(port)
+		);
+}
+
+static inline uint32_t inl(uint16_t port)
+{
+	uint32_t v;
+
+	asm volatile (
+		"inl %1, %0"
+		:"=a" (v)
+		:"dN"(port)
+		);
+	return v;
+}
+
+static inline void outl(uint32_t v, uint16_t port)
+{
+	asm volatile (
+		"outl %0, %1"
+		:
+		:"a" (v), "dN"(port)
+		);
+}
+
 #endif
