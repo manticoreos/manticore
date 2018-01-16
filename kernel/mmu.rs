e72a7bd4d50e9df045db16510ed771ee086a33ab
Pekka Enberg <penberg@iki.fi>
2018-01-16 21:06:47 +0200

    kernel/mmu: Export mmu_current_map() function

--- kernel/mmu.rs
+++ kernel/mmu.rs
@@ -7,6 +7,7 @@ pub const MMU_PROT_EXEC: usize = 1 << 2;
 pub const MMU_USER_PAGE: usize = 1 << 0;
 
 extern "C" {
+    pub fn mmu_current_map() -> MMUMap;
     pub fn mmu_map_range(map: MMUMap, vaddr: usize, paddr: usize, sz: usize, prot: usize, flags: usize) -> i32;
     pub fn mmu_invalidate_tlb();
     pub fn virt_to_phys(addr: usize) -> usize;
