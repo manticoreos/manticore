d512fae7393585691320138d80dccfcb1cd5b5bf
Pekka Enberg <penberg@iki.fi>
2018-01-17 17:52:54 +0200

    kernel/initrd: Start initrd process

--- kernel/initrd.c
+++ kernel/initrd.c
@@ -7,11 +7,17 @@
 void *initrd_start;
 void *initrd_end;
 
+extern void process_run(const char *image_start, unsigned long image_size);
+
 void initrd_load(void)
 {
 	if (!initrd_start) {
 		printf("No initrd found.\n");
 		return;
 	}
-	printf("Found initrd at %x (%d bytes)\n", initrd_start, initrd_end - initrd_start);
+	unsigned long initrd_size = initrd_end - initrd_start;
+
+	printf("Found initrd at %x (%d bytes)\n", initrd_start, initrd_size);
+
+	process_run(initrd_start, initrd_size);
 }
