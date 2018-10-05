0ff56c2c97328fdb1622240a6f9b8398ea519b6e
Pekka Enberg <penberg@iki.fi>
2018-10-05 06:58:12 +0300

    x86/mmu: Fix mmu_map_range() for ranges without large pages
    When mapping a range that is not covered by a large page, "large_start" (start
    aligned up) will be larger than "large start" (end aligned down) and both are
    potentially over range bounds.
    
    Fix the issue by detecting mappings that are not covered by large pages
    and map everything as small pages instead.

--- arch/x86_64/mmu.c
+++ arch/x86_64/mmu.c
@@ -234,31 +234,45 @@ int mmu_map_range(mmu_map_t map, virt_t vaddr, phys_t paddr, size_t size, mmu_pr
 	virt_t end = vaddr + size;
 	virt_t large_start = align_up(vaddr, PAGE_SIZE_LARGE);
 	virt_t large_end = align_down(vaddr + size, PAGE_SIZE_LARGE);
-	if (start != large_start) {
-		for (virt_t offset = start; offset < large_start; offset += PAGE_SIZE_SMALL) {
+	if (large_end < large_start) {
+		/* No large pages: */
+		for (virt_t offset = start; offset < end; offset += PAGE_SIZE_SMALL) {
 			int err = mmu_map_small_page(map, offset, paddr, prot, flags);
 			if (err) {
 				return err;
 			}
 			paddr += PAGE_SIZE_SMALL;
 		}
-	}
-	if (large_start != large_end) {
-		for (virt_t offset = large_start; offset < large_end; offset += PAGE_SIZE_LARGE) {
-			int err = mmu_map_large_page(map, offset, paddr, prot, flags);
-			if (err) {
-				return err;
+	} else {
+		/* First map any small pages before the large pages: */
+		if (start != large_start) {
+			for (virt_t offset = start; offset < large_start; offset += PAGE_SIZE_SMALL) {
+				int err = mmu_map_small_page(map, offset, paddr, prot, flags);
+				if (err) {
+					return err;
+				}
+				paddr += PAGE_SIZE_SMALL;
 			}
-			paddr += PAGE_SIZE_LARGE;
 		}
-	}
-	if (large_end != end) {
-		for (virt_t offset = large_end; offset < end; offset += PAGE_SIZE_SMALL) {
-			int err = mmu_map_small_page(map, offset, paddr, prot, flags);
-			if (err) {
-				return err;
+		/* Then map the large pages: */
+		if (large_start != large_end) {
+			for (virt_t offset = large_start; offset < large_end; offset += PAGE_SIZE_LARGE) {
+				int err = mmu_map_large_page(map, offset, paddr, prot, flags);
+				if (err) {
+					return err;
+				}
+				paddr += PAGE_SIZE_LARGE;
 			}
-			paddr += PAGE_SIZE_SMALL;
+		}
+		/* Finally, map left-over small pages: */
+		if (large_end != end) {
+			for (virt_t offset = large_end; offset < end; offset += PAGE_SIZE_SMALL) {
+				int err = mmu_map_small_page(map, offset, paddr, prot, flags);
+				if (err) {
+					return err;
+				}
+				paddr += PAGE_SIZE_SMALL;
+			}
 		}
 	}
 	return 0;
