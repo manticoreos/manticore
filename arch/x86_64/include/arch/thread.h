c5e6c7e21a5dd0d3577956da66780f87dd760520
Pekka Enberg <penberg@iki.fi>
2019-04-11 19:10:45 +0300

    x86/thread: Improve task switching
    In preparation for process scheduling, improve task switching code.

--- arch/x86_64/include/arch/thread.h
+++ arch/x86_64/include/arch/thread.h
@@ -1,14 +1,27 @@
 #ifndef X86_THREAD_H
 #define X86_THREAD_H
 
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
+
+#define __TIF_NEW	0
+#define TIF_NEW		(1ULL << __TIF_NEW)
 
 struct task_state {
 	void *rsp;
 	void *rip;
+	uint32_t flags;
 };
 
+struct task_state *task_state_new(void *rip, void *rsp);
+void task_state_delete(struct task_state *task_state);
 void task_state_init(struct task_state *task_state, void *rip, void *rsp);
+void *task_state_stack_top(struct task_state *task_state);
+void *task_state_entry_point(struct task_state *task_state);
+
 void switch_to(struct task_state *old, struct task_state *new);
+void switch_to_first(struct task_state *task_state);
+void switch_to_userspace(void *rip, void *rsp);
 
 #endif
--- arch/x86_64/thread.c
+++ arch/x86_64/thread.c
@@ -1,9 +1,43 @@
 #include <arch/thread.h>
 
+#include <kernel/kmem.h>
+
 void task_state_init(struct task_state *task_state, void *rip, void *rsp)
 {
 	task_state->rsp = rsp;
 	task_state->rip = rip;
+}
+
+struct task_state *task_state_new(void *rip, void *rsp)
+{
+	struct task_state *ret = kmem_alloc(sizeof(struct task_state));
+	if (ret) {
+		ret->flags = TIF_NEW;
+		ret->rip = rip;
+		ret->rsp = rsp;
+	}
+	return ret;
+}
+
+void task_state_delete(struct task_state *task_state)
+{
+	kmem_free(task_state, sizeof(struct task_state));
+}
+
+void *task_state_stack_top(struct task_state *task_state)
+{
+	return task_state->rsp;
+}
+
+void *task_state_entry_point(struct task_state *task_state)
+{
+	return task_state->rip;
+}
+
+void switch_to_first(struct task_state *task_state)
+{
+	task_state->flags &= ~TIF_NEW;
+	switch_to_userspace(task_state->rip, task_state->rsp);
 }
 
 void switch_to(struct task_state *old, struct task_state *new)
@@ -12,15 +46,20 @@ void switch_to(struct task_state *old, struct task_state *new)
 		"push	%%rbp\n"
 		"movq	$0f, %c[rip](%0)\n"
 		"movq	%%rsp, %c[rsp](%0)\n"
-		"movq	%c[rsp](%1), %%rsp\n"
-		"jmpq	*%c[rip](%1)\n"
+		"btrl	%[tif_new], %c[flags](%1)\n"
+		"movq	%c[rip](%1), %%rdi\n"
+		"movq	%c[rsp](%1), %%rsi\n"
+		"jc	switch_to_userspace\n"
+		"movq	%%rsi, %%rsp\n"
+		"jmpq	*%%rdi\n"
 		"0:\n"
 		"pop	%%rbp\n"
 		:
 		: "D"(old), "S"(new),
 		  [rsp]"i"(offsetof(struct task_state, rsp)),
-		  [rip]"i"(offsetof(struct task_state, rip))
-		: "rax", "rbx", "rcx", "rdx", "rbp", "rsp",
-		  "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15",
+		  [rip]"i"(offsetof(struct task_state, rip)),
+		  [tif_new]"i"(__TIF_NEW),
+		  [flags]"i"(offsetof(struct task_state, flags))
+		: "rax", "rbx", "rcx", "rdx", "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15",
 		  "memory", "flags", "cc");
 }
