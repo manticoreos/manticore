f4b6982a3b130b5e2023bd711a0acf48a79f5b5e
Pekka Enberg <penberg@iki.fi>
2019-05-11 08:55:31 +0300

    x86/apic: Periodic timer support
    This adds periodic timer support. We need something to wake up processes
    that block on the wait() system call. A periodic timer interrupt is
    sufficient for now. However, we eventually want to turn the timer
    interrupt in a variable period one-shot timer to avoid waking up the CPU
    needlessly, but that's for (much) later.

--- arch/x86_64/apic.c
+++ arch/x86_64/apic.c
@@ -5,6 +5,7 @@
 #include <arch/msr.h>
 
 #include <kernel/printf.h>
+#include <kernel/irq.h>
 
 #include <stdbool.h>
 
@@ -15,9 +16,13 @@ enum {
 	APIC_SPIV = 0x80f,
 	APIC_LVT_TIMER  = 0x832,
 	ACPI_LVT_LINT0  = 0x835,
-	APIC_TIMER_IC = 0x838,
-	APIC_TIMER_CC = 0x839,
-	APIC_TIMER_DC  = 0x83e,
+	APIC_TIMER_IC = 0x838,		// Timer initial count (IC) register
+	APIC_TIMER_CC = 0x839,		// Timer current count (DC) register
+	APIC_TIMER_DC  = 0x83e,		// Timer divide configuration (DD) register
+};
+
+enum {
+	APIC_LVT_TIMER_PERIODIC = 0x01 << 17,
 };
 
 /* MSI message data register flags. Specified in Section 10.11.2
@@ -45,6 +50,12 @@ static void apic_write(uint32_t addr, uint64_t val)
 	wrmsr(addr, val);
 }
 
+static void apic_timer_intr(void *arg)
+{
+	/* An interrupt wakes up the kernel from its idle loop that blocks on
+	   an arch_halt_cpu() call.  */
+}
+
 static void apic_eoi(void)
 {
 	apic_write(APIC_EOI, 0);
@@ -53,6 +64,15 @@ static void apic_eoi(void)
 void end_of_interrupt(void)
 {
 	apic_eoi();
+}
+
+static void apic_timer_init(void)
+{
+	irq_vector_t vector = request_irq(apic_timer_intr, NULL);
+	apic_write(APIC_LVT_TIMER, vector | APIC_LVT_TIMER_PERIODIC);
+	apic_write(APIC_TIMER_DC, 0x3);
+	/* TODO: Configure timer counter using unit of time. */
+	apic_write(APIC_TIMER_IC, 100000000);
 }
 
 static inline bool probe_x2apic(void)
@@ -74,4 +94,6 @@ void init_apic(void)
 	wrmsr(X86_IA32_APIC_BASE, _apic_base | X86_IA32_APIC_BASE_EXTD | X86_IA32_APIC_BASE_EN);
 	apic_write(ACPI_LVT_LINT0, 0);
 	apic_write(APIC_SPIV, 0x1ff);
+
+	apic_timer_init();
 }
