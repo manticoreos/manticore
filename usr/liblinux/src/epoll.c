c626a63073ace8bba44bf8c26ffbb92b972ea4d2
Pekka Enberg <penberg@iki.fi>
2019-08-18 17:12:39 +0300

    liblinux: Use getevents() in epoll_wait()
    This implements parts of epoll_wait() using getevents(). A process that
    calls epoll_wait() to query for events is now woken up and notified of
    the events. There's still work to do to turn the events into something
    usable to the application. We don't, for example, emit the data, just
    the event.

--- usr/liblinux/src/epoll.c
+++ usr/liblinux/src/epoll.c
@@ -1,6 +1,9 @@
 #include <sys/epoll.h>
 
 #include <errno.h>
+
+#include <manticore/atomic-ring-buffer.h>
+#include <manticore/events.h>
 #include <manticore/syscalls.h>
 
 #define EPOLL_FD	200
@@ -46,6 +49,8 @@ int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
 
 int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
 {
+	int err;
+
 	if (epfd != EPOLL_FD) {
 		errno = EBADF;
 		return -1;
@@ -54,7 +59,37 @@ int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
 		errno = EINVAL;
 		return -1;
 	}
-	wait();
-	// TODO: Fill the events array and return number of events.
-	return 0;
+
+	err = wait();
+	if (err) {
+		errno = -err;
+		return -1;
+	}
+
+	struct atomic_ring_buffer *queue;
+	err = getevents((void **)&queue);
+	if (err) {
+		errno = -err;
+		return -1;
+	}
+
+	int nr_events = 0;
+	while (!atomic_ring_buffer_is_empty(queue) && nr_events < maxevents) {
+		struct event *kern_event = atomic_ring_buffer_front(queue);
+		uint32_t ep_events = 0;
+
+		switch (kern_event->type) {
+		case EVENT_PACKET_IO:
+			ep_events = EPOLLIN;
+			break;
+		default:
+			break;
+		}
+
+		struct epoll_event *ep_event = &events[nr_events++];
+		ep_event->events = ep_events;
+
+		atomic_ring_buffer_pop(queue, sizeof(struct event));
+	}
+	return nr_events;
 }
