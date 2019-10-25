1d4a971b171286e0d4f34192203de4d1914ebb56
Tapasweni Pathak <tapaswenipathak@gmail.com>
2019-10-25 09:36:58 +0300

    include/kernel/align.h: is_aligned() API documentation

--- include/kernel/align.h
+++ include/kernel/align.h
@@ -4,6 +4,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/// Check if \size is aligned to \align.
+///
+/// \param size  The size to check for alignment.
+/// \param align The alignment to check for.
+/// \returns true if \size is aligned to \align; otherwise returns false.
 static inline bool is_aligned(uint64_t size, uint64_t align)
 {
 	return (size % align) == 0;
