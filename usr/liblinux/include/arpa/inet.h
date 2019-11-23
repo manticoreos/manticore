bbe416dcade2155b18ffb5e342031422ee78585a
Pekka Enberg <penberg@iki.fi>
2019-11-23 22:14:12 +0200

    liblinux, arpa/inet.h: Make ntoh*() and hton*() functions static inline
    We aim to use these functions in packet processing fast-paths. Let's,
    therefore, make the functions static inline so that the compiler can
    inline the byte order swapping logic to callers.

--- usr/liblinux/include/arpa/inet.h
+++ usr/liblinux/include/arpa/inet.h
@@ -3,9 +3,40 @@
 
 #include <inttypes.h>
 
-uint32_t htonl(uint32_t hostlong);
-uint16_t htons(uint16_t hostshort);
-uint32_t ntohl(uint32_t netlong);
-uint16_t ntohs(uint16_t netshort);
+static inline uint32_t htonl(uint32_t hostlong)
+{
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+	return hostlong;
+#else
+	return __builtin_bswap32(hostlong);
+#endif
+}
+
+static inline uint16_t htons(uint16_t hostshort)
+{
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+	return hostshort;
+#else
+	return __builtin_bswap16(hostshort);
+#endif
+}
+
+static inline uint32_t ntohl(uint32_t netlong)
+{
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+	return netlong;
+#else
+	return __builtin_bswap16(netlong);
+#endif
+}
+
+static inline uint16_t ntohs(uint16_t netshort)
+{
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+	return netshort;
+#else
+	return __builtin_bswap16(netshort);
+#endif
+}
 
 #endif
