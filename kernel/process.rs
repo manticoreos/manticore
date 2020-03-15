9ac76a9efbec6d7430fc8ca8a76c4333906ccddd
Pekka Enberg <penberg@iki.fi>
2020-03-15 09:54:51 +0200

    kernel: Move ELF parsing to separate function
    Clean up process_spawn() by moving ELF parsing to parse_elf_image() function.

--- kernel/process.rs
+++ kernel/process.rs
@@ -84,28 +84,8 @@ pub unsafe extern "C" fn process_spawn(image_start: *const u8, image_size: usize
     let mmu_map = mmu::mmu_current_map();
     let mut vmspace = VMAddressSpace::new(mmu_map);
 
-    let buf = slice::from_raw_parts(image_start, image_size);
-    let elf_file = ElfFile::new(&buf).unwrap();
-    let ph_iter = elf_file.program_iter();
-    for phdr in ph_iter {
-        match phdr.get_type().unwrap() {
-            program::Type::Load => {
-                let prot = elf_phdr_flags_to_prot(phdr.flags());
-                let start = phdr.virtual_addr() as usize;
-                let size = phdr.mem_size() as usize;
-                let end = memory::align_up((start + size) as u64, memory::PAGE_SIZE_SMALL) as usize;
-                vmspace.allocate(start, end, prot).expect("allocate failed");
-                let image_start: u64 = transmute(image_start);
-                let src_start: u64 = image_start + phdr.offset();
-                let src_end = src_start + phdr.file_size();
-                vmspace.populate_from(start, end, src_start as usize, src_end as usize).expect("populate_from failed");
-            }
-            _ => {}
-        }
-    }
-    if let Some(section) = elf_file.find_section_by_name(".bss") {
-        memset(transmute(section.address()), 0, section.size() as usize);
-    }
+    let entry_point = parse_elf_image(image_start, image_size, &mut vmspace);
+
     let stack_top = 0x40000000;
     let stack_size = memory::PAGE_SIZE_LARGE as usize;
     let stack_start = stack_top - stack_size;
@@ -128,11 +108,37 @@ pub unsafe extern "C" fn process_spawn(image_start: *const u8, image_size: usize
     vmspace.populate(io_buf_start, io_buf_end).expect("populate failed");
     let io_queue = IOQueue::new(io_buf_start, io_buf_size);
 
-    let task_state = task_state_new(elf_file.header.pt2.entry_point() as usize, stack_top);
+    let task_state = task_state_new(entry_point, stack_top);
 
     let process = Rc::new(Process::new(task_state, vmspace, event_queue, io_queue));
 
     sched::enqueue(process);
+}
+
+fn parse_elf_image(image_start: *const u8, image_size: usize, vmspace: &mut VMAddressSpace) -> usize {
+    let buf = unsafe { slice::from_raw_parts(image_start, image_size) };
+    let elf_file = ElfFile::new(&buf).unwrap();
+    let ph_iter = elf_file.program_iter();
+    for phdr in ph_iter {
+        match phdr.get_type().unwrap() {
+            program::Type::Load => {
+                let prot = elf_phdr_flags_to_prot(phdr.flags());
+                let start = phdr.virtual_addr() as usize;
+                let size = phdr.mem_size() as usize;
+                let end = memory::align_up((start + size) as u64, memory::PAGE_SIZE_SMALL) as usize;
+                vmspace.allocate(start, end, prot).expect("allocate failed");
+                let image_start: u64 = unsafe { transmute(image_start) };
+                let src_start: u64 = image_start + phdr.offset();
+                let src_end = src_start + phdr.file_size();
+                vmspace.populate_from(start, end, src_start as usize, src_end as usize).expect("populate_from failed");
+            }
+            _ => {}
+        }
+    }
+    if let Some(section) = elf_file.find_section_by_name(".bss") {
+        unsafe { memset(transmute(section.address()), 0, section.size() as usize) };
+    }
+    return elf_file.header.pt2.entry_point() as usize;
 }
 
 fn elf_phdr_flags_to_prot(flags: program::Flags) -> VMProt {
