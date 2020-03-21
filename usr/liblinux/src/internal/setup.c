9fb3823b5346e3aa54de0fdb582ab6d620abf475
Pekka Enberg <penberg@iki.fi>
2020-03-21 12:13:04 +0200

    liblinux: Add __liblinux_setup() function to crt0
    This adds a __liblinux_setup() function to crt0, which is called before
    the main() function for liblinux internal setup.

---
+++ usr/liblinux/src/internal/setup.c
@@ -0,0 +1,5 @@
+#include "internal/setup.h"
+
+void __liblinux_setup(void)
+{
+}
---
+++ usr/liblinux/src/internal/setup.h
@@ -0,0 +1,6 @@
+#ifndef __LIBLINUX_INTERNAL_SETUP_H
+#define __LIBLINUX_INTERNAL_SETUP_H
+
+void __liblinux_setup(void);
+
+#endif
