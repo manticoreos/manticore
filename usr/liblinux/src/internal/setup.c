f7ea9c510c0e7772ecf94a31e21977bf1bc4377d
Pekka Enberg <penberg@iki.fi>
2020-03-21 12:14:36 +0200

    liblinux: Store MAC address in __liblinux_mac_address
    Query for the MAC address in __liblinux_setup() and store it in
    __liblinux_mac_address so that the user space network stack can use it.

--- usr/liblinux/src/internal/setup.c
+++ usr/liblinux/src/internal/setup.c
@@ -1,5 +1,17 @@
 #include "internal/setup.h"
 
+#include <manticore/config_abi.h>
+#include <manticore/syscalls.h>
+
+#include <stdio.h>
+
+char __liblinux_mac_addr[ETH_ALEN];
+
 void __liblinux_setup(void)
 {
+	get_config("/dev/eth", CONFIG_ETHERNET_MAC_ADDRESS, __liblinux_mac_addr, ETH_ALEN);
+
+	fprintf(stderr, "liblinux: MAC address is %02x:%02x:%02x:%02x:%02x:%02x\n", __liblinux_mac_addr[0],
+		__liblinux_mac_addr[1], __liblinux_mac_addr[2], __liblinux_mac_addr[3], __liblinux_mac_addr[4],
+		__liblinux_mac_addr[5]);
 }
--- usr/liblinux/src/internal/setup.h
+++ usr/liblinux/src/internal/setup.h
@@ -1,6 +1,10 @@
 #ifndef __LIBLINUX_INTERNAL_SETUP_H
 #define __LIBLINUX_INTERNAL_SETUP_H
 
+#include <linux/if_ether.h>
+
 void __liblinux_setup(void);
+
+extern char __liblinux_mac_addr[ETH_ALEN];
 
 #endif
