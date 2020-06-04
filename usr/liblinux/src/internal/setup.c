60210fd00959d70131e0b6b4e463a4487dfcf8cd
Pekka Enberg <penberg@iki.fi>
2020-06-04 19:58:47 +0300

    liblinux: Ethernet I/O queue lookup

--- usr/liblinux/src/internal/setup.c
+++ usr/liblinux/src/internal/setup.c
@@ -9,13 +9,19 @@
 
 char __liblinux_mac_addr[ETH_ALEN];
 
+io_queue_t __liblinux_eth_ioqueue;
+
 void __liblinux_setup(void)
 {
 	int eth_desc = acquire("/dev/eth", 0);
 	if (eth_desc < 0) {
 		assert(0);
 	}
+
+	get_config(eth_desc, CONFIG_IO_QUEUE, &__liblinux_eth_ioqueue, sizeof(io_queue_t));
+
 	get_config(eth_desc, CONFIG_ETHERNET_MAC_ADDRESS, __liblinux_mac_addr, ETH_ALEN);
+
 	fprintf(stderr, "MAC address = %02x:%02x:%02x:%02x:%02x:%02x\n", __liblinux_mac_addr[0], __liblinux_mac_addr[1],
 		__liblinux_mac_addr[2], __liblinux_mac_addr[3], __liblinux_mac_addr[4], __liblinux_mac_addr[5]);
 }
--- usr/liblinux/src/internal/setup.h
+++ usr/liblinux/src/internal/setup.h
@@ -1,9 +1,13 @@
 #ifndef __LIBLINUX_INTERNAL_SETUP_H
 #define __LIBLINUX_INTERNAL_SETUP_H
 
+#include <manticore/io_queue.h>
+
 #include <linux/if_ether.h>
 
 void __liblinux_setup(void);
+
+extern io_queue_t __liblinux_eth_ioqueue;
 
 extern char __liblinux_mac_addr[ETH_ALEN];
 
