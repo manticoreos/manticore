1355304c0e1533941f150241c31596f51417e83b
Pekka Enberg <penberg@iki.fi>
2020-06-07 17:30:43 +0300

    usr/echod: Fix error message when socket() fails

--- usr/echod/echod.c
+++ usr/echod/echod.c
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd < 0) {
-		die("sockfd");
+		die("socket");
 	}
 	struct sockaddr_in addr = {
 		.sin_family = AF_INET,
